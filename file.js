6dc3d39ed2504dc61e34d46170f27af931efe49c
Damien Keith Delgadillo <39660748+Damien-Delgadillo@users.noreply.github.com>
2019-09-01 17:01:38 -0700

    Introduced new js code, and specified node v in package.json

--- file.js
+++ file.js
@@ -1,2 +1,5 @@
 //file.js
-var PORT = process.env.PORT || 3000;
+//var PORT = process.env.PORT || 3000;
+app.listen(process.env.PORT || 3000, function(){
+        console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
