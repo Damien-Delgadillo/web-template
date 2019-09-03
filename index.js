24aec6330d8d39e3c717da5be6b5b6274df751cc
Damien Keith Delgadillo <39660748+Damien-Delgadillo@users.noreply.github.com>
2019-09-03 03:18:35 -0700

    minor change to index.js

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 //index.js
 const express = require('express');
-const app = express(); 
+const app = express();
 const path = require('path');
 const port = process.env.PORT || 5500;
 
+app.use(express.static(__dirname));
+
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
+  res.sendFile(path.join(__dirname, '/index.html'));
 });
 
 app.listen(port);
